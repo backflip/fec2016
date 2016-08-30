80196da63b647a899516c29780500c3b2db16839
Thomas Jaggi <thomas.jaggi@unic.com>
2016-08-30 18:57:17 +0200

    Clean up client code

--- client/main.js
+++ client/main.js
@@ -1,32 +1,24 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-// import path from 'path';
 
 import './main.html';
 
-// OSM = new Mongo.Collection('OSM');
-
-var aCv, bCv, aTrigger, bTrigger;
 function getOrCreateOSMObject(address) {
   var data = {address: address};
-  var foo = OSM.findOne(data);
+  var OSMObject = OSM.findOne(data);
 
-  if (!foo) {
-    Meteor.call('OSM.insert', address)
-    foo = OSM.findOne(data);
+  if (!OSMObject) {
+    Meteor.call('OSM.insert', address);
+
+    OSMObject = OSM.findOne(data);
   }
 
-  return foo;
+  return OSMObject;
 }
 
+// Template.app.onCreated(function helloOnCreated() {
+// });
 
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
+Template.app.helpers({
   aCv() {
     var aCv = OSM.findOne({address: '/osm/a/cv'}) || {}
     var value = aCv.value || 0;
@@ -39,10 +31,11 @@ Template.hello.helpers({
   }
 });
 
-Template.hello.events({
+Template.app.events({
   'input input'(event, instance) {
     var value = parseFloat(event.currentTarget.value, 10);
     var aCv = getOrCreateOSMObject('/osm/a/cv');
+    
     Meteor.call('OSM.update', {
       _id: aCv._id,
       address: aCv.address,
@@ -51,6 +44,7 @@ Template.hello.events({
   },
   'click .a-tr'(event, instance) {
     var aTr = getOrCreateOSMObject('/osm/a/tr');
+
     Meteor.call('OSM.trigger', {
       _id: aTr._id,
       address: aTr.address
